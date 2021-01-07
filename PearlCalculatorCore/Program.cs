3c43545a86e0975dc1911ab99392e21ee4858884
LegendsOfSky <legendsofskylos@gmail.com>
2021-01-08 04:26:55 +0800

    An Empty Project
    Just an empty project

---
+++ PearlCalculatorCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PearlCalculatorLib;
+
+namespace PearlCalculatorCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PublicData data = new PublicData();
+            Console.ReadKey();
+        }
+    }
+}
