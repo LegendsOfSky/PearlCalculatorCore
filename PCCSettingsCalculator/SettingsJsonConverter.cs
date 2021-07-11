0ddaf15a55a116722283a3fe984c07f7f047ee2c
LegendsOfSky <legendsofskylos@gmail.com>
2021-07-12 06:53:40 +0800

    SG : Added missing settings and  clear screen feature before asking for next parameter

--- PCCSettingsCalculator/SettingsJsonConverter.cs
+++ PCCSettingsCalculator/SettingsJsonConverter.cs
@@ -12,14 +12,14 @@ namespace PCCSettingsGenerator
     public class SettingsJsonConverter : JsonConverter<Settings>
     {
         private Space3DJsonConverter _space3DJsonConverter = new Space3DJsonConverter();
-        
-        public override Settings Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
+        public override Settings Read(ref Utf8JsonReader reader , Type typeToConvert , JsonSerializerOptions options)
         {
             using var document = JsonDocument.ParseValue(ref reader);
             var rootElement = document.RootElement;
 
             var settings = new Settings();
-            
+
             settings.NorthEastTNT = ReadSpace3D(rootElement.GetProperty(nameof(settings.NorthEastTNT)));
             settings.NorthWestTNT = ReadSpace3D(rootElement.GetProperty(nameof(settings.NorthWestTNT)));
             settings.SouthEastTNT = ReadSpace3D(rootElement.GetProperty(nameof(settings.SouthEastTNT)));
@@ -32,66 +32,89 @@ namespace PCCSettingsGenerator
             settings.BlueTNT = rootElement.GetProperty(nameof(settings.BlueTNT)).GetInt32();
             settings.MaxTNT = rootElement.GetProperty(nameof(settings.MaxTNT)).GetInt32();
 
-            settings.Direction =
-                Enum.TryParse<Direction>(rootElement.GetProperty(nameof(settings.Direction)).GetString(),
-                    out var direction)
-                    ? direction
-                    : Direction.North;
-            
+            {
+                settings.Direction =
+                    Enum.TryParse<Direction>(rootElement.GetProperty(nameof(settings.Direction)).GetString() ,
+                        out var direction)
+                        ? direction
+                        : Direction.North;
+            }
+
             var pearlElemRoot = rootElement.GetProperty(nameof(settings.Pearl));
 
             settings.Pearl = new PearlEntity
             {
-                Position = ReadSpace3D(pearlElemRoot.GetProperty(nameof(settings.Pearl.Position))),
+                Position = ReadSpace3D(pearlElemRoot.GetProperty(nameof(settings.Pearl.Position))) ,
                 Motion = ReadSpace3D(pearlElemRoot.GetProperty(nameof(settings.Pearl.Motion)))
             };
-            
+
+            {
+                settings.DefaultRedTNTDirection =
+                    rootElement.TryGetProperty(nameof(settings.DefaultRedTNTDirection) , out var drt) &&
+                    Enum.TryParse<Direction>(drt.GetString() , out var direction)
+                        ? direction
+                        : Data.DefaultRedDuper;
+            }
+
+            {
+                settings.DefaultBlueTNTDirection =
+                    rootElement.TryGetProperty(nameof(settings.DefaultBlueTNTDirection) , out var drt) &&
+                    Enum.TryParse<Direction>(drt.GetString() , out var direction)
+                        ? direction
+                        : Data.DefaultBlueDuper;
+            }
+
             return settings;
         }
 
-        public override void Write(Utf8JsonWriter writer, Settings value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer , Settings value , JsonSerializerOptions options)
         {
             writer.WriteStartObject();
 
-            WriteObject(writer, options, nameof(value.NorthEastTNT), ref value.NorthEastTNT, _space3DJsonConverter);
+            writer.WriteString("Version" , Settings.Version);
 
-            WriteObject(writer, options, nameof(value.NorthWestTNT), ref value.NorthWestTNT, _space3DJsonConverter);
+            WriteObject(writer , options , nameof(value.NorthEastTNT) , ref value.NorthEastTNT , _space3DJsonConverter);
 
-            WriteObject(writer, options, nameof(value.SouthEastTNT), ref value.SouthEastTNT, _space3DJsonConverter);
+            WriteObject(writer , options , nameof(value.NorthWestTNT) , ref value.NorthWestTNT , _space3DJsonConverter);
 
-            WriteObject(writer, options, nameof(value.SouthWestTNT), ref value.SouthWestTNT, _space3DJsonConverter);
-            
-            WriteObject(writer, options, nameof(value.Destination), ref value.Destination, _space3DJsonConverter);
+            WriteObject(writer , options , nameof(value.SouthEastTNT) , ref value.SouthEastTNT , _space3DJsonConverter);
 
-            WriteObject(writer, options, nameof(value.Offset), ref value.Offset, o =>
+            WriteObject(writer , options , nameof(value.SouthWestTNT) , ref value.SouthWestTNT , _space3DJsonConverter);
+
+            WriteObject(writer , options , nameof(value.Destination) , ref value.Destination , _space3DJsonConverter);
+
+            WriteObject(writer , options , nameof(value.Offset) , ref value.Offset , o =>
             {
-                writer.WriteNumber("X", o.X);
-                writer.WriteNumber("Z", o.Z);
+                writer.WriteNumber("X" , o.X);
+                writer.WriteNumber("Z" , o.Z);
             });
-            
-            WriteObject(writer, options, nameof(value.Pearl), ref value.Pearl, o =>
+
+            WriteObject(writer , options , nameof(value.Pearl) , ref value.Pearl , o =>
             {
-                WriteObject(writer, options, nameof(o.Position), ref o.Position, _space3DJsonConverter);
-                WriteObject(writer, options, nameof(o.Motion), ref o.Motion, _space3DJsonConverter);
+                WriteObject(writer , options , nameof(o.Position) , ref o.Position , _space3DJsonConverter);
+                WriteObject(writer , options , nameof(o.Motion) , ref o.Motion , _space3DJsonConverter);
             });
-            
-            writer.WriteNumber(nameof(value.RedTNT), value.RedTNT);
-            writer.WriteNumber(nameof(value.BlueTNT), value.BlueTNT);
-            writer.WriteNumber(nameof(value.MaxTNT), value.MaxTNT);
-            
-            writer.WriteString(nameof(value.Direction), value.Direction.ToString());
-            
+
+            writer.WriteNumber(nameof(value.RedTNT) , value.RedTNT);
+            writer.WriteNumber(nameof(value.BlueTNT) , value.BlueTNT);
+            writer.WriteNumber(nameof(value.MaxTNT) , value.MaxTNT);
+
+            writer.WriteString(nameof(value.Direction) , value.Direction.ToString());
+
+            writer.WriteString(nameof(value.DefaultRedTNTDirection) , value.DefaultRedTNTDirection.ToString());
+            writer.WriteString(nameof(value.DefaultBlueTNTDirection) , value.DefaultBlueTNTDirection.ToString());
+
             writer.WriteEndObject();
         }
 
-        private void WriteObject<T>(Utf8JsonWriter writer, JsonSerializerOptions options, string propertyName, ref T value, JsonConverter<T> converter)
+        private void WriteObject<T>(Utf8JsonWriter writer , JsonSerializerOptions options , string propertyName , ref T value , JsonConverter<T> converter)
         {
             writer.WriteStartObject(propertyName);
-            converter.Write(writer, value, options);
+            converter.Write(writer , value , options);
             writer.WriteEndObject();
         }
-        
-        private void WriteObject<T>(Utf8JsonWriter writer, JsonSerializerOptions options, string propertyName, ref T value, Action<T> action)
+
+        private void WriteObject<T>(Utf8JsonWriter writer , JsonSerializerOptions options , string propertyName , ref T value , Action<T> action)
         {
             writer.WriteStartObject(propertyName);
             action?.Invoke(value);
@@ -102,8 +125,8 @@ namespace PCCSettingsGenerator
         {
             return new Space3D
             {
-                X = elem.GetProperty("X").GetDouble(),
-                Y = elem.GetProperty("Y").GetDouble(),
+                X = elem.GetProperty("X").GetDouble() ,
+                Y = elem.GetProperty("Y").GetDouble() ,
                 Z = elem.GetProperty("Z").GetDouble()
             };
         }
@@ -112,7 +135,7 @@ namespace PCCSettingsGenerator
         {
             return new Surface2D
             {
-                X = elem.GetProperty("X").GetDouble(),
+                X = elem.GetProperty("X").GetDouble() ,
                 Z = elem.GetProperty("Z").GetDouble()
             };
         }
@@ -120,16 +143,16 @@ namespace PCCSettingsGenerator
 
     class Space3DJsonConverter : JsonConverter<Space3D>
     {
-        public override Space3D Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override Space3D Read(ref Utf8JsonReader reader , Type typeToConvert , JsonSerializerOptions options)
         {
             throw new NotImplementedException();
         }
 
-        public override void Write(Utf8JsonWriter writer, Space3D value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer , Space3D value , JsonSerializerOptions options)
         {
-            writer.WriteNumber("X", value.X);
-            writer.WriteNumber("Y", value.Y);
-            writer.WriteNumber("Z", value.Z);
+            writer.WriteNumber("X" , value.X);
+            writer.WriteNumber("Y" , value.Y);
+            writer.WriteNumber("Z" , value.Z);
         }
     }
 }
