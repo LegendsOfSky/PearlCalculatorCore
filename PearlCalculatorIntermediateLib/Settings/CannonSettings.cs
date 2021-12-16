0fad6e6d21b9a449f62a7cb6bbe873239b8265d4
LegendsOfSky <legendsofskylos@gmail.com>
2021-12-16 23:11:43 +0800

    Predefine Settings Content

---
+++ PearlCalculatorIntermediateLib/Settings/CannonSettings.cs
@@ -0,0 +1,30 @@
+﻿using PearlCalculatorLib.PearlCalculationLib.Entity;
+using PearlCalculatorLib.PearlCalculationLib.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearlCalculatorIntermediateLib.Settings
+{
+    [Serializable]
+    public class CannonSettings
+    {
+        public string CannonName { get; set; }
+
+        public int MaxTNT { get; set; }
+
+        public Direction DefaultRedDirection { get; set; }
+        public Direction DefaultBlueDirection  { get; set; }
+        
+        public Space3D NorthWestTNT  { get; set; }
+        public Space3D NorthEastTNT  { get; set; }
+        public Space3D SouthWestTNT { get; set; }
+        public Space3D SouthEastTNT { get; set; }
+
+        public PearlEntity Pearl  { get; set; }
+        public Surface2D Offset { get; set; }
+    }
+}
---
+++ PearlCalculatorIntermediateLib/Settings/SettingsColletion.cs
@@ -0,0 +1,29 @@
+﻿using PearlCalculatorLib.PearlCalculationLib.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearlCalculatorIntermediateLib.Settings
+{
+    [Serializable]
+    public class SettingsColletion
+    {
+        public double Version { get; set; } = 2.71;
+
+        public int RedTNT { get; set; }
+
+        public int BlueTNT { get; set; }
+
+        public string CannonName { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public Surface2D? Destination { get; set; }
+
+        public Surface2D? CannonLocation { get; set; }
+
+        public CannonSettings[] CannonSettings { get; set; }
+    }
+}
